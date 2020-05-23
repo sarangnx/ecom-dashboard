9e412d263d283cfb35fee4dc87cb4cf1fcc5a987
Sarang <sarang98divakaran@gmail.com>
2020-05-23 22:04:11 +0530

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <notifications></notifications>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        const api = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
+        const server = process.env.VUE_APP_SERVER_URL || 'http://localhost:3000';
+
+        this.$store.commit('setUrl', api);
+        this.$store.commit('setServerUrl', server);
+    },
+};
+</script>
