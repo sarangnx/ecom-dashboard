3a63aa58f4b7ec5e727c3d1330edf2b333cd6bd6
Sarang <sarang98divakaran@gmail.com>
2020-05-24 20:35:20 +0530

    login page

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7">
+            <div class="card bg-secondary shadow border-0">
+                <div class="card-body px-lg-5 py-lg-5">
+                    <div class="text-center text-muted mb-4">
+                        Sign in to Dashboard
+                    </div>
+                    <form role="form" method="post" action="#" @submit.prevent="login">
+                        <base-input
+                            v-model="username"
+                            class="input-group-alternative mb-3"
+                            placeholder="Email"
+                            addon-left-icon="envelope-open"
+                            :error="$v.username.$error ? 'Email Required' : null"
+                        >
+                        </base-input>
+
+                        <base-input
+                            v-model="password"
+                            class="input-group-alternative"
+                            placeholder="Password"
+                            type="password"
+                            addon-left-icon="lock"
+                            :error="$v.password.$error ? 'Password Required' : null"
+                        >
+                        </base-input>
+
+                        <base-checkbox class="custom-control-alternative">
+                            <span class="text-muted">Remember me</span>
+                        </base-checkbox>
+                        <div class="text-center">
+                            <base-button type="primary" class="my-4" @click.prevent="login">
+                                Sign in
+                            </base-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-6">
+                    <a href="#" class="text-light"><small>Forgot password?</small></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { required } from 'vuelidate/lib/validators';
+
+export default {
+    name: 'Login',
+    data: () => ({
+        username: '',
+        password: '',
+        loading: false,
+    }),
+    validations: {
+        username: {
+            required,
+        },
+        password: {
+            required,
+        },
+    },
+    methods: {
+        async login() {
+            this.$v.$touch();
+
+            // Send request once the form is valid
+            if (!this.$v.$invalid) {
+                this.loading = true;
+
+                try {
+                    const loggedIn = await this.$store.dispatch('login/login', {
+                        username: this.username,
+                        password: this.password,
+                    });
+
+                    if (loggedIn) {
+                        this.$router.push('/');
+                    }
+                } catch (err) {
+                    this.$error('Something went Wrong!');
+                }
+
+                this.loading = false;
+            } else {
+                this.$error('All fields required.');
+            }
+        },
+    },
+};
+</script>
