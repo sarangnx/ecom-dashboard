fb0cb49ea390e2fc4ac6f939c8107b94a873913a
Sarang <sarang98divakaran@gmail.com>
2020-05-28 21:37:24 +0530

    category tree component and index

---
+++ src/views/Category/components/CategoryNode.vue
@@ -0,0 +1,22 @@
+<template>
+    <li>
+        <div>
+            {{ item.categoryName }}
+        </div>
+        <ul v-if="item.subCategory && item.subCategory.length">
+            <category-node v-for="(category, index) in item.subCategory" :key="index" :item="category" />
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'CategoryNode',
+    props: {
+        item: {
+            type: Object,
+            default: () => [],
+        },
+    },
+};
+</script>
---
+++ src/views/Category/components/CategoryTree.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="category-tree">
+        <ul>
+            <template v-for="(item, index) in items">
+                <category-node :key="index" :item="item" />
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+import CategoryNode from './CategoryNode';
+
+export default {
+    name: 'CategoryTree',
+    components: {
+        CategoryNode,
+    },
+    props: {
+        items: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
