8eb132187f52292146ec5895766b76cb39c88648
Sarang <sarang98divakaran@gmail.com>
2020-05-29 15:50:05 +0530

    add category button & pass event to parent

--- src/views/Category/components/CategoryTree.vue
+++ src/views/Category/components/CategoryTree.vue
@@ -2,8 +2,20 @@
     <div class="category-tree">
         <ul>
             <template v-for="(item, index) in items">
-                <category-node :key="index" :item="item" />
+                <category-node :key="index" :item="item" @add-category="$emit('add-category', $event)" />
             </template>
+            <li class="category-node">
+                <base-button
+                    size="sm"
+                    :class="'ml-3'"
+                    bg-color="#000"
+                    text-color="#fff"
+                    title="Add Category"
+                    @click="$emit('add-category', null)"
+                >
+                    <font-awesome-icon icon="plus" />
+                </base-button>
+            </li>
         </ul>
     </div>
 </template>
