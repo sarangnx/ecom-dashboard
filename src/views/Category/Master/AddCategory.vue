8d7002ee0a3c334e432c4f6c35f305a939476fa4
Sarang <sarang98divakaran@gmail.com>
2020-05-29 21:16:32 +0530

    upload method

--- src/views/Category/Master/AddCategory.vue
+++ src/views/Category/Master/AddCategory.vue
@@ -71,11 +71,49 @@ export default {
             this.$refs.file.value = this.$refs.file.defaultValue;
             this.$refs.image.innerHTML = 'Category Thumbnail';
         },
-        upload() {
+        async upload() {
             this.$v.$touch();
             if (this.$v.$invalid) return;
 
             this.loading = true;
+
+            const data = {
+                categoryName: this.categoryName,
+                image: this.image,
+                parentCategoryId: this.parent && this.parent.categoryId,
+            };
+
+            // Wrap it as FormData.
+            const formData = new FormData();
+            Object.keys(data).forEach((key) => {
+                formData.append(key, data[key]);
+            });
+
+            try {
+                const response = await this.$axios({
+                    method: 'post',
+                    url: '/category/master',
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                    data: formData,
+                });
+
+                if (response.status === 200 && response.data.message) {
+                    this.$success(response.data.message);
+                }
+
+                // reset form
+                this.categoryName = null;
+                this.removeImage();
+            } catch (err) {
+                if (err.response && err.response.status === 400 && err.response.data.error) {
+                    this.$error(err.response.data.error.message);
+                } else {
+                    this.$error('Something went wrong. Please try again later.');
+                }
+            } finally {
+                this.$v.$reset();
+                this.loading = false;
+            }
         },
     },
 };
