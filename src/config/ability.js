b00c399929e08462829f3e546c63a375bc94b8fa
Sarang <sarang98divakaran@gmail.com>
2020-06-01 21:53:49 +0530

    added category rules

--- src/config/ability.js
+++ src/config/ability.js
@@ -1,32 +1,29 @@
+/**
+ * Rules
+ *  - action 'menu' is for filtering out dashboard navbar items.
+ *  - if a navbar item with children are to be allowed, both
+ *    parent and child subjects are to be included.
+ */
 export default {
     guest: [],
     user: [],
     delivery: [],
     staff: [],
     shopowner: [
-        {
-            action: 'menu',
-            subject: 'store-dashboard',
-        },
-        {
-            action: 'menu',
-            subject: 'orders',
-        },
-        {
-            action: 'menu',
-            subject: 'items',
-        },
+        { action: 'menu', subject: 'store-dashboard' },
+        { action: 'menu', subject: 'orders' },
+        { action: 'menu', subject: 'items' },
+        { action: 'menu', subject: 'category' }, // parent
+        { action: 'menu', subject: 'store-category' }, // child
     ],
     admin: [
-        {
-            action: 'menu',
-            subject: 'admin-dashboard',
-        },
+        { action: 'menu', subject: 'admin-dashboard' },
+        { action: 'menu', subject: 'category' },
+        { action: 'menu', subject: 'store-category' },
     ],
     superadmin: [
-        {
-            action: 'menu',
-            subject: 'admin-dashboard',
-        },
+        { action: 'menu', subject: 'admin-dashboard' },
+        { action: 'menu', subject: 'category' },
+        { action: 'menu', subject: 'store-category' },
     ],
 };
