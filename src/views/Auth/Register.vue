81674ba9257642e274daa41bafa172ad13c2fda6
Sarang <sarang98divakaran@gmail.com>
2020-06-07 14:42:45 +0530

    Register UI

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -2,64 +2,50 @@
     <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
             <div class="card bg-secondary shadow border-0">
-                <div class="card-header bg-transparent pb-5">
-                    <div class="text-muted text-center mt-2 mb-3">
-                        <small>Sign up with</small>
-                    </div>
-                    <div class="btn-wrapper text-center">
-                        <a href="#" class="btn btn-neutral btn-icon">
-                            <span class="btn-inner--icon"><img src="img/icons/common/github.svg" /></span>
-                            <span class="btn-inner--text">Github</span>
-                        </a>
-                        <a href="#" class="btn btn-neutral btn-icon">
-                            <span class="btn-inner--icon"><img src="img/icons/common/google.svg" /></span>
-                            <span class="btn-inner--text">Google</span>
-                        </a>
-                    </div>
-                </div>
                 <div class="card-body px-lg-5 py-lg-5">
-                    <div class="text-center text-muted mb-4">
-                        <small>Or sign up with credentials</small>
+                    <div class="card-header text-center bg-transparent">
+                        <small>Register</small>
                     </div>
-                    <form role="form">
+                    <form autocomplete="false">
                         <base-input
-                            v-model="model.name"
-                            class="input-group-alternative mb-3"
-                            placeholder="Name"
-                            addon-left-icon="ni ni-hat-3"
-                        >
-                        </base-input>
-
-                        <base-input
-                            v-model="model.email"
-                            class="input-group-alternative mb-3"
+                            v-model="$v.username.$model"
+                            classes="input-group-alternative"
                             placeholder="Email"
-                            addon-left-icon="ni ni-email-83"
+                            addon-left-icon="envelope-open"
+                            autocomplete="false"
+                            :error="$v.username.$error ? 'Email Required' : null"
                         >
                         </base-input>
 
                         <base-input
-                            v-model="model.password"
-                            class="input-group-alternative"
+                            v-model="$v.password.$model"
+                            classes="input-group-alternative"
                             placeholder="Password"
                             type="password"
-                            addon-left-icon="ni ni-lock-circle-open"
+                            addon-left-icon="lock"
+                            autocomplete="false"
+                            :error="
+                                $v.password.$error && !$v.password.required
+                                    ? 'Password Required'
+                                    : $v.password.$error && !$v.password.minLength
+                                    ? 'Minimum 8 Characters Required'
+                                    : null
+                            "
+                        >
+                        </base-input>
+                        <base-input
+                            v-model="$v.confirmPassword.$model"
+                            classes="input-group-alternative"
+                            placeholder="Confirm Password"
+                            type="password"
+                            addon-left-icon="lock"
+                            autocomplete="false"
+                            :error="$v.confirmPassword.$error ? `Passwords don't match` : null"
                         >
                         </base-input>
 
-                        <div class="text-muted font-italic">
-                            <small>password strength: <span class="text-success font-weight-700">strong</span></small>
-                        </div>
-
-                        <div class="row my-4">
-                            <div class="col-12">
-                                <base-checkbox class="custom-control-alternative">
-                                    <span class="text-muted">I agree with the <a href="#!">Privacy Policy</a></span>
-                                </base-checkbox>
-                            </div>
-                        </div>
                         <div class="text-center">
-                            <base-button type="primary" class="my-4">Create account</base-button>
+                            <base-button type="primary" class="my-4" @click="register">Create account</base-button>
                         </div>
                     </form>
                 </div>
@@ -80,16 +66,32 @@
     </div>
 </template>
 <script>
+import { required, sameAs, minLength } from 'vuelidate/lib/validators';
+
 export default {
     name: 'Register',
-    data() {
-        return {
-            model: {
-                name: '',
-                email: '',
-                password: '',
-            },
-        };
+    data: () => ({
+        username: '',
+        password: '',
+        confirmPassword: '',
+    }),
+    validations: {
+        username: {
+            required,
+        },
+        password: {
+            required,
+            minLength: minLength(8),
+        },
+        confirmPassword: {
+            required,
+            sameAsPassword: sameAs('password'),
+        },
+    },
+    methods: {
+        register() {
+            console.log(this.$v);
+        },
     },
 };
 </script>
