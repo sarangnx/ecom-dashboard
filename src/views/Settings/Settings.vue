c6e3635ecdaaab457e71da5924e053391b39c736
Sarang <sarang98divakaran@gmail.com>
2020-06-08 13:19:20 +0530

    Settings list view

---
+++ src/views/Settings/Settings.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <base-button type="link" @click="$router.push('/settings/stores')">Profile</base-button>
+        </div>
+        <div class="col-12">
+            <base-button type="link" @click="$router.push('/settings/stores')">Stores</base-button>
+        </div>
+    </div>
+</template>
