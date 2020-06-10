7860e185371a025198b1e7274125421818889113
Sarang <sarang98divakaran@gmail.com>
2020-06-10 15:25:53 +0530

    address

--- src/views/Orders/OrdersTable.vue
+++ src/views/Orders/OrdersTable.vue
@@ -7,7 +7,46 @@
             <div v-for="(order, index) in orders" :key="index" class="card shadow h-100 col-md-5 mb-3 p-4">
                 <div class="d-flex text-muted">
                     <h4 class="m-0 pr-2 text-muted">Order Date</h4>
-                    <span>{{ formatDate(order.orderDate) }}</span>
+                    <small>{{ formatDate(order.orderDate) }}</small>
+                </div>
+                <div class="d-flex align-items-center">
+                    <h4 class="m-0 pr-2">Name</h4>
+                    <span v-if="order.user && order.user.firstName">
+                        {{ order.user.firstName }}
+                    </span>
+                    <small v-else class="text-muted">Not Provided</small>
+                </div>
+                <div class="d-flex flex-column">
+                    <h4 class="m-0 pr-2">Address</h4>
+                    <div v-if="order.deliveryAddress" class="d-flex flex-column">
+                        <span v-if="order.deliveryAddress.house">
+                            <small class="text-muted font-weight-bold">House:</small>
+                            {{ order.deliveryAddress.house }},
+                        </span>
+                        <span v-if="order.deliveryAddress.area">
+                            <small class="text-muted font-weight-bold">Area:</small>
+                            {{ order.deliveryAddress.area }},
+                        </span>
+                        <span v-if="order.deliveryAddress.city">
+                            <small class="text-muted font-weight-bold">City:</small>
+                            {{ order.deliveryAddress.city }},
+                        </span>
+                        <span v-if="order.deliveryAddress.landmark">
+                            <small class="text-muted font-weight-bold">Landmark:</small>
+                            {{ order.deliveryAddress.landmark }}
+                        </span>
+                        <span v-if="order.deliveryAddress.district">
+                            <small class="text-muted font-weight-bold">District:</small>
+                            {{ order.deliveryAddress.district }}
+                        </span>
+                        <span v-if="order.deliveryAddress.pincode">
+                            <small class="text-muted font-weight-bold">Pincode:</small>
+                            {{ order.deliveryAddress.pincode }}
+                        </span>
+                    </div>
+                    <div v-else class="d-flex flex-column">
+                        <small class="text-muted">Not Provided</small>
+                    </div>
                 </div>
             </div>
         </div>
@@ -39,6 +78,7 @@ export default {
 
             const orders = response.data.orders;
             this.orders = orders.rows;
+            console.log(this.orders);
         },
         formatDate(date) {
             date = new Date(date);
