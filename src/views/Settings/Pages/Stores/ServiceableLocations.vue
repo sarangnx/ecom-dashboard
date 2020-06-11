a7a9e1e0cc4813ae8f999483c4a62edb47a25b65
Sarang <sarang98divakaran@gmail.com>
2020-06-11 20:16:08 +0530

    remove pincodes

--- src/views/Settings/Pages/Stores/ServiceableLocations.vue
+++ src/views/Settings/Pages/Stores/ServiceableLocations.vue
@@ -25,7 +25,7 @@
                 </a>
             </base-dropdown>
         </div>
-        <div class="col-12 mt-3 d-flex flex-wrap">
+        <div v-if="selectedPincodes && selectedPincodes.length" class="col-12 mt-3 d-flex flex-wrap">
             <badge
                 v-for="(pincode, index) in selectedPincodes"
                 :key="index"
@@ -34,8 +34,11 @@
                 <span class="px-2 text-capitalize font-weight-normal">
                     <strong>{{ pincode.pincode }}</strong> - {{ pincode.officeName }}
                 </span>
-                <base-button icon="times" size="sm" />
+                <base-button icon="times" size="sm" @click="remove(pincode)" />
             </badge>
+        </div>
+        <div v-else class="col-12 mt-3 text-center py-5">
+            <small>Add Pincodes from the dropdown list above.</small>
         </div>
     </div>
 </template>
@@ -54,6 +57,7 @@ export default {
     },
     data: () => ({
         storePincodes: [],
+        removedPincodes: [],
         newPincodes: [],
         filter: null,
         filteredPincodes: [],
@@ -89,6 +93,21 @@ export default {
 
             const pincodes = response.data.pincodes;
             this.storePincodes = pincodes.rows;
+        },
+        remove(pincode) {
+            let index = this.newPincodes.findIndex((item) => item.pinId === pincode.pinId);
+            // if pincode to be removed is in newPincodes just splice
+            if (index !== -1) {
+                this.newPincodes.splice(index, 1);
+                return;
+            }
+
+            index = this.storePincodes.findIndex((item) => item.pinId === pincode.pinId);
+            // if it is in storePincodes, add it to removedPincodes list
+            if (index !== -1) {
+                const removed = this.storePincodes.splice(index, 1);
+                this.removedPincodes.push(removed);
+            }
         },
     },
 };
