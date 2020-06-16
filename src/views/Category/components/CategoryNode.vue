dc8810271f7bbc7388be8d827cb11e3e283378e0
Sarang <sarang98divakaran@gmail.com>
2020-06-16 19:02:27 +0530

    new context menu using dropdown

--- src/views/Category/components/CategoryNode.vue
+++ src/views/Category/components/CategoryNode.vue
@@ -10,17 +10,32 @@
             <span class="mr-3 text-dark font-weight-bold">
                 {{ item.categoryName }}
             </span>
-            <base-button v-show="showOption" size="sm" type="primary">
-                <font-awesome-icon icon="ellipsis-v" fixed-width />
-            </base-button>
+            <base-dropdown v-show="showOption">
+                <base-button slot="title" size="sm" type="primary">
+                    <font-awesome-icon icon="ellipsis-v" fixed-width />
+                </base-button>
+                <a class="dropdown-item pointer" @click="$emit('add-category', item.categoryId)">
+                    <font-awesome-icon icon="plus" />
+                    <strong>Add Sub Category</strong>
+                </a>
+                <a class="dropdown-item pointer" @click="$emit('edit-category', item.categoryId)">
+                    <font-awesome-icon icon="edit" />
+                    <strong>Edit Category</strong>
+                </a>
+                <a class="dropdown-item pointer" @click="$emit('delete-category', item.categoryId)">
+                    <font-awesome-icon icon="trash" />
+                    <strong>Delete Category</strong>
+                </a>
+            </base-dropdown>
         </div>
-        <ul v-if="item.subCategory && item.subCategory.length && open">
+        <ul v-if="item.subCategory && item.subCategory.length && open" class="category-list">
             <category-node
                 v-for="(category, index) in item.subCategory"
                 :key="index"
                 :item="category"
                 @add-category="$emit('add-category', $event)"
-                @menu:open="relay"
+                @edit-category="$emit('edit-category', $event)"
+                @delete-category="$emit('delete-category', $event)"
             />
         </ul>
     </li>
@@ -50,9 +65,6 @@ export default {
     methods: {
         toggleOpen() {
             this.open = !this.open;
-        },
-        relay(event, data) {
-            this.$emit('menu:open', event, data);
         },
     },
 };
