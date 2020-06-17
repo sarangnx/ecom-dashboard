49dbf7b65498d3dbc2f63f02deec32fac0e96290
Sarang <sarang98divakaran@gmail.com>
2020-06-17 14:15:40 +0530

    Password fields

--- src/views/Settings/Pages/Profile/Password.vue
+++ src/views/Settings/Pages/Profile/Password.vue
@@ -1,10 +1,50 @@
 <template>
     <div class="card shadow">
-        <div class="card-header d-flex justify-content-between">
-            <router-link to="/settings"><h3>Settings</h3></router-link>
+        <div class="card-header d-flex justify-content-start align-items-center">
+            <base-button size="sm" icon="arrow-left" @click="$router.push('/settings')"></base-button>
+            <h3 class="m-0">Change Password</h3>
         </div>
-        <div class="card-body d-flex flex-row justify-content-start flex-wrap">
-            <div class="container"></div>
+        <div class="card-body bg-secondary d-flex flex-row justify-content-start flex-wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <base-input
+                            v-model="oldPassword"
+                            type="password"
+                            label="Old Password"
+                            input-classes="form-control-alternative"
+                        />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <base-input
+                            v-model="newPassword"
+                            type="password"
+                            label="New Password"
+                            input-classes="form-control-alternative"
+                        />
+                    </div>
+                    <div class="col-lg-6">
+                        <base-input
+                            v-model="repeatPassword"
+                            type="password"
+                            label="Repeat Password"
+                            input-classes="form-control-alternative"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    name: 'ChangePassword',
+    data: () => ({
+        oldPassword: null,
+        newPassword: null,
+        repeatPassword: null,
+    }),
+};
+</script>
