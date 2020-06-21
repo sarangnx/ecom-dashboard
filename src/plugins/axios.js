1ffcadae4f2d46d00d336873df04934a1be7e0b4
Sarang <sarang98divakaran@gmail.com>
2020-06-21 20:56:58 +0530

    added axios interceptor for handling 401

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import router from '@/router';
 
 export default {
     install(Vue) {
         Vue.prototype.$axios = axios;
         Vue.prototype.$axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+        Vue.prototype.$axios.interceptors.response.use(
+            function (response) {
+                return response;
+            },
+            function (error) {
+                if (error.response.status === 401) {
+                    router.push('/login').catch(() => {});
+                }
+
+                return error;
+            }
+        );
     },
 };
