a42cd33772bb36a5822e6240ed43720b857fabdc
Sarang <sarang98divakaran@gmail.com>
2020-06-26 13:37:54 +0530

    image selector

--- src/views/Services/Pages/components/AddService.vue
+++ src/views/Services/Pages/components/AddService.vue
@@ -14,6 +14,38 @@
                 <textarea v-model="service.description" class="form-control" rows="3" style="resize: none;"></textarea>
             </div>
             <div class="col-12 mt-3">
+                <h5>Thumbnail</h5>
+                <div class="row">
+                    <input ref="file" type="file" class="hidden" accept="image/*" @change="loadImageFile($event)" />
+                    <div class="col-12">
+                        <div v-show="service.image" class="image-container">
+                            <img ref="image" src="#" class="col-6" />
+                            <!-- Overlay -->
+                            <div class="image-overlay col-6">
+                                <div class="d-flex justify-content-center align-items-center height">
+                                    <base-button type="success" icon="camera" @click="openImage()">
+                                        <small>Change</small>
+                                    </base-button>
+                                    <base-button type="danger" icon="trash" @click.prevent="removeImage()" />
+                                </div>
+                            </div>
+                        </div>
+                        <div v-show="!service.image" class="input-group">
+                            <div class="custom-file">
+                                <input
+                                    ref="file"
+                                    type="file"
+                                    class="custom-file-input"
+                                    accept="image/*"
+                                    @change="loadImageFile($event)"
+                                />
+                                <label ref="label" class="custom-file-label">Thumbnail</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mt-3">
                 <base-button block icon="plus" @click="addService">Add Service</base-button>
             </div>
         </div>
@@ -28,7 +60,9 @@ import { required } from 'vuelidate/lib/validators';
 export default {
     data: () => ({
         loading: false,
-        service: {},
+        service: {
+            image: null,
+        },
     }),
     validations: {
         service: {
@@ -40,6 +74,29 @@ export default {
     methods: {
         addService() {
             console.log(this.service);
+        },
+        removeImage() {
+            // remove selected image from buffer and data property of vue.
+            // and set label to default.
+            this.service.image = null;
+            this.$refs.file.value = this.$refs.file.defaultValue;
+            this.$refs.label.innerHTML = 'Thumbnail';
+        },
+        loadImageFile(event) {
+            this.service.image = event.target.files[0];
+            if (event.target.files[0]) {
+                let reader = new FileReader();
+
+                reader.onload = (e) => {
+                    this.$refs.image.setAttribute('src', e.target.result);
+                };
+
+                reader.readAsDataURL(event.target.files[0]);
+            }
+        },
+        openImage() {
+            // open the file selector.
+            this.$refs.file.click();
         },
     },
 };
