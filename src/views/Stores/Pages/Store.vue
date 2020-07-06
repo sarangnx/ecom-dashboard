0d98721f8403d76267b52812b86c1f48513d3ef4
Sarang <sarang98divakaran@gmail.com>
2020-07-06 21:09:05 +0530

    component to view store

---
+++ src/views/Stores/Pages/Store.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card-body p-0 position-relative min__height"></div>
+</template>
+<script>
+export default {
+    props: {
+        store: {
+            type: Object,
+            default: () => {},
+        },
+    },
+    mounted() {
+        console.log(this.store);
+    },
+};
+</script>
