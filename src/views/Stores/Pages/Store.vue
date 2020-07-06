98c9508ec2b737c88cbce50ce2edab76d9c7f93c
Sarang <sarang98divakaran@gmail.com>
2020-07-06 21:15:50 +0530

    store name & type

--- src/views/Stores/Pages/Store.vue
+++ src/views/Stores/Pages/Store.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="card-body p-0 position-relative min__height"></div>
+    <div class="card-body position-relative min__height">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <small class="font-weight-bold mr-2">Store Name:</small>
+                    <span>{{ store.name }}</span>
+                </div>
+                <div class="col-12">
+                    <small class="font-weight-bold mr-2">Store Type:</small>
+                    <span>{{ storeTypes[store.storeType] }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -9,6 +22,16 @@ export default {
             default: () => {},
         },
     },
+    data: () => ({
+        storeTypes: {
+            SUPERMARKET: 'Super Market',
+            GROCERY: 'Grocery',
+            RESTAURANTS: 'Restaurants',
+            MEDICALSHOPS: 'Medical Shops',
+            VEGNFRUITS: 'Vegetables & Fruits',
+            OTHERS: 'Others',
+        },
+    }),
     mounted() {
         console.log(this.store);
     },
