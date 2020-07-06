dc34b9c2e0977d6073a0f9bbb83e46c8804c1ea6
Sarang <sarang98divakaran@gmail.com>
2020-07-06 15:29:40 +0530

    table

--- src/views/Stores/Pages/Stores.vue
+++ src/views/Stores/Pages/Stores.vue
@@ -4,6 +4,74 @@
             <h3>Stores</h3>
         </div>
         <div class="card-body p-0 position-relative min__height">
+            <template v-if="stores && stores.length">
+                <div class="p-2 d-flex justify-content-between">
+                    <div>
+                        <small class="mr-1 font-weight-bold">Per Page:</small>
+                        <base-dropdown>
+                            <base-button slot="title" size="sm" icon="caret-down" icon-position="right">
+                                {{ perPage }}
+                            </base-button>
+                            <a class="dropdown-item" @click="perPage = 10">10</a>
+                            <a class="dropdown-item" @click="perPage = 50">50</a>
+                            <a class="dropdown-item" @click="perPage = 100">100</a>
+                        </base-dropdown>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <base-table
+                        class="table align-items-center table-flush service-table"
+                        thead-classes="thead-dark"
+                        :data="stores"
+                    >
+                        <template slot="columns">
+                            <th class="text-center text-white">
+                                <a
+                                    class="pointer"
+                                    @click.prevent="order === 'asc' ? (order = 'desc') : (order = 'asc')"
+                                >
+                                    Store Name
+                                    <font-awesome-icon :icon="order === 'asc' ? 'caret-up' : 'caret-down'" />
+                                </a>
+                            </th>
+                            <th class="text-center text-white">Blocked</th>
+                            <th class="text-center text-white">Verified</th>
+                            <th class="text-center text-white">Actions</th>
+                        </template>
+                        <template slot-scope="{ row }">
+                            <td class="text-center">
+                                <span>
+                                    {{ row.name }}
+                                </span>
+                            </td>
+                            <td class="text-center">
+                                <badge :type="row.blocked ? 'danger' : 'success'">
+                                    {{ row.blocked ? 'Blocked' : 'Active' }}
+                                </badge>
+                            </td>
+                            <td class="text-center">
+                                <badge :type="row.verified ? 'success' : 'danger'">
+                                    {{ row.verified ? 'Verified' : 'Not Verified' }}
+                                </badge>
+                            </td>
+                            <td>
+                                <div class="d-flex justify-content-center">
+                                    <base-button type="primary" icon="eye" size="sm" title="View Details"></base-button>
+                                    <base-button
+                                        :type="row.blocked ? 'success' : 'warning'"
+                                        :icon="row.blocked ? 'user' : 'user-slash'"
+                                        size="sm"
+                                        :title="row.blocked ? 'Unblock Store' : 'Block Store'"
+                                        :loading="blocked && blocked.includes(row.storeId)"
+                                        :disabled="blocked && blocked.includes(row.storeId)"
+                                        @click="blockStore(row.storeId, !row.blocked)"
+                                    ></base-button>
+                                </div>
+                            </td>
+                        </template>
+                    </base-table>
+                </div>
+            </template>
             <div
                 v-if="!(stores && stores.length) && !loading"
                 class="col-12 p-5 d-flex justify-content-center flex-column align-items-center"
@@ -30,6 +98,8 @@ export default {
         perPage: 10,
         stores: null,
         totalPages: null,
+        order: 'asc',
+        blocked: [],
     }),
     mounted() {
         this.getStores();
@@ -62,6 +132,7 @@ export default {
 
             this.loading = false;
         },
+        async blockStore() {},
     },
 };
 </script>
