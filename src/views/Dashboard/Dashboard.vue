eb4d5c23f7290165fe48cd0727ac02e0aec5ae96
Sarang <sarang98divakaran@gmail.com>
2020-07-10 15:41:36 +0530

    order stats view

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-            <!-- Card stats -->
             <div class="row">
                 <div class="col-xl-3 col-lg-6">
                     <div class="card">
@@ -10,6 +9,40 @@
                                 Order Stats
                                 <font-awesome-icon icon="shopping-basket" class="text-primary" pull="right" />
                             </h3>
+                            <div class="text-sm row">
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Total Orders:</span>
+                                    <span>{{ totalOrders }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Total Orders Today:</span>
+                                    <span>{{ totalToday }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Pending:</span>
+                                    <span>{{ orderStatus && orderStatus.pending }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Processing:</span>
+                                    <span>{{ orderStatus && orderStatus.processing }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Ready:</span>
+                                    <span>{{ orderStatus && orderStatus.ready }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Out for Delivery:</span>
+                                    <span>{{ orderStatus && orderStatus.outfordelivery }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Delivered:</span>
+                                    <span>{{ orderStatus && orderStatus.delivered }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Cancelled:</span>
+                                    <span>{{ orderStatus && orderStatus.cancelled }}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -24,7 +57,7 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <base-table :data="most_sold" type="hover table-striped table-sm">
+                                <!-- <base-table :data="most_sold" type="hover table-striped table-sm">
                                     <template slot="columns">
                                         <th>#</th>
                                         <th>Item</th>
@@ -37,15 +70,15 @@
                                         </td>
                                         <td>
                                             <img v-if="row.item_details.image_path" class="item-image" />
-                                            <i v-else class="fa fa-image"></i>
-                                            <!-- Alt Image -->
-                                            {{ row.item_details.item_name }}
+                                            <i v-else class="fa fa-image"></i> -->
+                                <!-- Alt Image -->
+                                <!-- {{ row.item_details.item_name }}
                                         </td>
                                         <td>
                                             {{ parseInt(row.occurance) }}
                                         </td>
                                     </template>
-                                </base-table>
+                                </base-table> -->
                                 <!-- Table -->
                             </div>
                             <!-- responsive-table -->
