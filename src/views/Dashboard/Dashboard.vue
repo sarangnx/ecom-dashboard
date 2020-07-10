1f4a73d247dc32f59854225d1c46601e9661229b
Sarang <sarang98divakaran@gmail.com>
2020-07-10 15:54:42 +0530

    alt screen for new users:

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,74 +1,75 @@
 <template>
     <div>
-        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-            <div v-if="current" class="mb-3 d-flex">
-                <h3 class="mr-2">Store:</h3>
-                <base-dropdown>
-                    <base-button slot="title" type="default" size="sm" class="dropdown-toggle">
-                        {{ current.name }}
-                    </base-button>
-                    <a v-for="(item, index) in stores" :key="index" class="dropdown-item" @click="change(item)">
-                        {{ item.name }}
-                    </a>
-                </base-dropdown>
-            </div>
-            <div class="row">
-                <div class="col-xl-3 col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h3>
-                                Order Stats
-                                <font-awesome-icon icon="shopping-basket" class="text-primary" pull="right" />
-                            </h3>
-                            <div class="text-sm row">
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Total Orders:</span>
-                                    <span>{{ totalOrders }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Total Orders Today:</span>
-                                    <span>{{ totalToday }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Pending:</span>
-                                    <span>{{ orderStatus && orderStatus.pending }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Processing:</span>
-                                    <span>{{ orderStatus && orderStatus.processing }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Ready:</span>
-                                    <span>{{ orderStatus && orderStatus.ready }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Out for Delivery:</span>
-                                    <span>{{ orderStatus && orderStatus.outfordelivery }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Delivered:</span>
-                                    <span>{{ orderStatus && orderStatus.delivered }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Cancelled:</span>
-                                    <span>{{ orderStatus && orderStatus.cancelled }}</span>
+        <template v-if="storeId">
+            <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+                <div v-if="current" class="mb-3 d-flex">
+                    <h3 class="mr-2">Store:</h3>
+                    <base-dropdown>
+                        <base-button slot="title" type="default" size="sm" class="dropdown-toggle">
+                            {{ current.name }}
+                        </base-button>
+                        <a v-for="(item, index) in stores" :key="index" class="dropdown-item" @click="change(item)">
+                            {{ item.name }}
+                        </a>
+                    </base-dropdown>
+                </div>
+                <div class="row">
+                    <div class="col-xl-3 col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3>
+                                    Order Stats
+                                    <font-awesome-icon icon="shopping-basket" class="text-primary" pull="right" />
+                                </h3>
+                                <div class="text-sm row">
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Total Orders:</span>
+                                        <span>{{ totalOrders }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Total Orders Today:</span>
+                                        <span>{{ totalToday }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Pending:</span>
+                                        <span>{{ orderStatus && orderStatus.pending }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Processing:</span>
+                                        <span>{{ orderStatus && orderStatus.processing }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Ready:</span>
+                                        <span>{{ orderStatus && orderStatus.ready }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Out for Delivery:</span>
+                                        <span>{{ orderStatus && orderStatus.outfordelivery }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Delivered:</span>
+                                        <span>{{ orderStatus && orderStatus.delivered }}</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="mr-2 font-weight-bold">Cancelled:</span>
+                                        <span>{{ orderStatus && orderStatus.cancelled }}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </base-header>
-        <div class="container-fluid mt--7">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card shadow">
-                        <div class="card-header d-flex justify-content-between">
-                            <h2>Most Sold Products</h2>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <!-- <base-table :data="most_sold" type="hover table-striped table-sm">
+            </base-header>
+            <div class="container-fluid mt--7">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card shadow">
+                            <div class="card-header d-flex justify-content-between">
+                                <h2>Most Sold Products</h2>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <!-- <base-table :data="most_sold" type="hover table-striped table-sm">
                                     <template slot="columns">
                                         <th>#</th>
                                         <th>Item</th>
@@ -82,17 +83,40 @@
                                         <td>
                                             <img v-if="row.item_details.image_path" class="item-image" />
                                             <i v-else class="fa fa-image"></i> -->
-                                <!-- Alt Image -->
-                                <!-- {{ row.item_details.item_name }}
+                                    <!-- Alt Image -->
+                                    <!-- {{ row.item_details.item_name }}
                                         </td>
                                         <td>
                                             {{ parseInt(row.occurance) }}
                                         </td>
                                     </template>
                                 </base-table> -->
-                                <!-- Table -->
+                                    <!-- Table -->
+                                </div>
+                                <!-- responsive-table -->
                             </div>
-                            <!-- responsive-table -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+        <div v-else>
+            <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"> </base-header>
+            <div class="container-fluid mt--7">
+                <div class="card shadow p-2 p-md-5">
+                    <div class="card-body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h3>You cannot view this page since you don't have any stores currently.</h3>
+                                    <div class="mb-3">
+                                        <span class="text-muted">You can return to this page after adding one.</span>
+                                    </div>
+                                    <base-button icon="external-link-alt" @click="$router.push('/settings/stores')">
+                                        Go to settings
+                                    </base-button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
