4896201a7c013a1dcd92b2f6129042e2864ab885
Sarang <sarang98divakaran@gmail.com>
2020-07-12 16:58:21 +0530

    masonry layout using card-columns

--- src/views/Dashboard/AdminDashboard.vue
+++ src/views/Dashboard/AdminDashboard.vue
@@ -2,54 +2,48 @@
     <div>
         <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
         <div class="container-fluid mt--7">
-            <div class="row">
-                <div class="col-12 col-lg-6 col-xl-3">
-                    <div class="card shadow-lg">
-                        <div class="card-header pb-0">
-                            <h3>
-                                User Stats
-                                <font-awesome-icon icon="users" class="text-primary" pull="right" />
-                            </h3>
-                        </div>
-                        <div class="card-body bg-secondary">
-                            <div class="text-sm row">
-                                <div v-for="(group, key) in userStatsText" :key="key" class="col-12">
-                                    <span class="mr-2 font-weight-bold">{{ group }}:</span>
-                                    <span>{{ userStats[key] || 0 }}</span>
-                                </div>
+            <div class="card-columns">
+                <div class="card shadow-lg">
+                    <div class="card-header pb-0">
+                        <h3>
+                            User Stats
+                            <font-awesome-icon icon="users" class="text-primary" pull="right" />
+                        </h3>
+                    </div>
+                    <div class="card-body bg-secondary">
+                        <div class="text-sm row">
+                            <div v-for="(group, key) in userStatsText" :key="key" class="col-12">
+                                <span class="mr-2 font-weight-bold">{{ group }}:</span>
+                                <span>{{ userStats[key] || 0 }}</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-lg-6 col-xl-3 mt-4 mt-lg-0 mt-xl-0">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h3>
-                                Total Stores
-                                <font-awesome-icon icon="store" class="text-primary" pull="right" />
-                            </h3>
-                            <span class="font-weight-bold">{{ storeStats.stores }}</span>
-                        </div>
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h3>
+                            Total Stores
+                            <font-awesome-icon icon="store" class="text-primary" pull="right" />
+                        </h3>
+                        <span class="font-weight-bold">{{ storeStats.stores }}</span>
                     </div>
                 </div>
-                <div class="col-12 col-lg-6 col-xl-3 mt-4 mt-xl-0">
-                    <div class="card shadow-lg">
-                        <div class="card-header pb-0">
-                            <h3>
-                                Inventory Stats
-                                <font-awesome-icon icon="drumstick-bite" class="text-primary" pull="right" />
-                            </h3>
-                        </div>
-                        <div class="card-body bg-secondary">
-                            <div class="text-sm row">
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Total Items:</span>
-                                    <span>{{ inventoryStats.items }}</span>
-                                </div>
-                                <div class="col-12">
-                                    <span class="mr-2 font-weight-bold">Total Categories:</span>
-                                    <span>{{ inventoryStats.categories }}</span>
-                                </div>
+                <div class="card shadow-lg">
+                    <div class="card-header pb-0">
+                        <h3>
+                            Inventory Stats
+                            <font-awesome-icon icon="drumstick-bite" class="text-primary" pull="right" />
+                        </h3>
+                    </div>
+                    <div class="card-body bg-secondary">
+                        <div class="text-sm row">
+                            <div class="col-12">
+                                <span class="mr-2 font-weight-bold">Total Items:</span>
+                                <span>{{ inventoryStats.items }}</span>
+                            </div>
+                            <div class="col-12">
+                                <span class="mr-2 font-weight-bold">Total Categories:</span>
+                                <span>{{ inventoryStats.categories }}</span>
                             </div>
                         </div>
                     </div>
@@ -123,3 +117,17 @@ export default {
     },
 };
 </script>
+<style lang="scss" scoped>
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+@import '~bootstrap/scss/mixins/_breakpoints';
+.card-columns {
+    column-count: 1;
+    @include media-breakpoint-up(md) {
+        column-count: 2;
+    }
+    @include media-breakpoint-up(lg) {
+        column-count: 3;
+    }
+}
+</style>
