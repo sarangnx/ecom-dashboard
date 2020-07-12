3a3039a09269d7de749324c1b00af0511457d440
Sarang <sarang98divakaran@gmail.com>
2020-07-12 16:28:53 +0530

    inventory stats

--- src/views/Dashboard/AdminDashboard.vue
+++ src/views/Dashboard/AdminDashboard.vue
@@ -21,14 +21,36 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-lg-6 col-xl-3">
+                <div class="col-12 col-lg-6 col-xl-3 mt-4 mt-lg-0 mt-xl-0">
                     <div class="card shadow-lg">
                         <div class="card-body">
                             <h3>
                                 Total Stores
-                                <font-awesome-icon icon="users" class="text-primary" pull="right" />
+                                <font-awesome-icon icon="store" class="text-primary" pull="right" />
                             </h3>
                             <span class="font-weight-bold">{{ storeStats.stores }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-xl-3 mt-4 mt-xl-0">
+                    <div class="card shadow-lg">
+                        <div class="card-header pb-0">
+                            <h3>
+                                Inventory Stats
+                                <font-awesome-icon icon="drumstick-bite" class="text-primary" pull="right" />
+                            </h3>
+                        </div>
+                        <div class="card-body bg-secondary">
+                            <div class="text-sm row">
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Total Items:</span>
+                                    <span>{{ inventoryStats.items }}</span>
+                                </div>
+                                <div class="col-12">
+                                    <span class="mr-2 font-weight-bold">Total Categories:</span>
+                                    <span>{{ inventoryStats.categories }}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,10 +72,12 @@ export default {
             service: 'Service Experts',
         },
         storeStats: {},
+        inventoryStats: {},
     }),
     mounted() {
         this.getUserStats();
         this.getStoreStats();
+        this.getInventoryStats();
     },
     methods: {
         async getUserStats() {
@@ -78,6 +102,20 @@ export default {
 
                 const stats = response.data.stats;
                 this.storeStats = stats;
+            } catch (err) {
+                this.$error('Unable to get stats.');
+            }
+        },
+        async getInventoryStats() {
+            try {
+                const response = await this.$axios({
+                    method: 'get',
+                    url: '/inventory/stats',
+                });
+
+                const stats = response.data.stats;
+                this.inventoryStats = stats;
+                console.log(stats);
             } catch (err) {
                 this.$error('Unable to get stats.');
             }
