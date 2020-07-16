bbdc7d2fdacd40f236c0249583e8fcb4cae2e3a5
Sarang <sarang98divakaran@gmail.com>
2020-07-16 19:03:38 +0530

    footer links:

--- src/layout/ContentFooter.vue
+++ src/layout/ContentFooter.vue
@@ -1,7 +1,15 @@
 <template>
     <footer class="footer bg-transparent px-4">
-        <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6">
+        <div class="row">
+            <div class="col-12 mb-3 text-center text-lg-left">
+                <router-link to="/" class="text-sm mr-4 font-weight-bold text-gray">
+                    Home
+                </router-link>
+                <router-link to="/terms" class="text-sm font-weight-bold text-gray">
+                    Terms
+                </router-link>
+            </div>
+            <div class="col-12">
                 <div class="copyright text-center text-lg-left text-muted">
                     © {{ year }}
                     <a href="https://www.doitnow.co.in" class="font-weight-bold ml-1" target="_blank">
