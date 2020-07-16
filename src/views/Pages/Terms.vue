b59a9acf28a11c30e15241e5c4d6774f254e873a
Sarang <sarang98divakaran@gmail.com>
2020-07-16 17:21:44 +0530

    terms card

--- src/views/Pages/Terms.vue
+++ src/views/Pages/Terms.vue
@@ -1,3 +1,22 @@
 <template>
-    <div></div>
+    <div class="container-fluid pt-8">
+        <div class="row justify-content-center">
+            <div class="col-10 col-lg-8 col-xl-6">
+                <div class="card shadow-lg">
+                    <div class="card-header border-0">
+                        <h2 class="text-center">Terms & Conditions</h2>
+                    </div>
+                    <div class="card-body">
+                        <terms-text />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import TermsText from './TermsText';
+export default {
+    components: { TermsText },
+};
+</script>
