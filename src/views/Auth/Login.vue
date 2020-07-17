feb8b7156321c4cbe97152e3ae8c198fab5f2abc
Sarang <sarang98divakaran@gmail.com>
2020-07-17 14:15:02 +0530

    login page changed to match new layout

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,58 +1,60 @@
 <template>
-    <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
-            <div class="card bg-secondary shadow border-0">
-                <div class="card-body px-lg-5 py-lg-5">
-                    <div class="text-center text-muted mb-4">
-                        Sign in to Dashboard
-                    </div>
-                    <form role="form" method="post" action="#" @submit.prevent="login">
-                        <base-input
-                            v-model="username"
-                            class="input-group-alternative mb-3"
-                            placeholder="Email"
-                            addon-left-icon="envelope-open"
-                            :error="$v.username.$error ? 'Email Required' : null"
-                        >
-                        </base-input>
-
-                        <base-input
-                            v-model="password"
-                            class="input-group-alternative"
-                            placeholder="Password"
-                            type="password"
-                            addon-left-icon="lock"
-                            :error="$v.password.$error ? 'Password Required' : null"
-                        >
-                        </base-input>
-
-                        <base-checkbox class="custom-control-alternative">
-                            <span class="text-muted">Remember me</span>
-                        </base-checkbox>
-                        <div class="text-center">
-                            <base-button
-                                type="primary"
-                                class="my-4"
-                                :loading="loading"
-                                :disabled="loading"
-                                @click.prevent="login"
-                            >
-                                Sign in
-                            </base-button>
+    <div class="container-fluid pt-8">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="text-center text-muted mb-4">
+                            Sign in to Dashboard
                         </div>
-                    </form>
+                        <form role="form" method="post" action="#" @submit.prevent="login">
+                            <base-input
+                                v-model="username"
+                                class="input-group-alternative mb-3"
+                                placeholder="Email"
+                                addon-left-icon="envelope-open"
+                                :error="$v.username.$error ? 'Email Required' : null"
+                            >
+                            </base-input>
+
+                            <base-input
+                                v-model="password"
+                                class="input-group-alternative"
+                                placeholder="Password"
+                                type="password"
+                                addon-left-icon="lock"
+                                :error="$v.password.$error ? 'Password Required' : null"
+                            >
+                            </base-input>
+
+                            <base-checkbox class="custom-control-alternative">
+                                <span class="text-muted">Remember me</span>
+                            </base-checkbox>
+                            <div class="text-center">
+                                <base-button
+                                    type="primary"
+                                    class="my-4"
+                                    :loading="loading"
+                                    :disabled="loading"
+                                    @click.prevent="login"
+                                >
+                                    Sign in
+                                </base-button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-6">
-                    <router-link to="/register" class="text-light">
-                        <small>Register an account.</small>
-                    </router-link>
-                </div>
-                <div class="col-6 text-right">
-                    <router-link to="/reset" class="text-light">
-                        <small>Forgot password?</small>
-                    </router-link>
+                <div class="row mt-3">
+                    <div class="col-6">
+                        <router-link to="/register" class="text-light">
+                            <small>Register an account.</small>
+                        </router-link>
+                    </div>
+                    <div class="col-6 text-right">
+                        <router-link to="/reset" class="text-light">
+                            <small>Forgot password?</small>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
