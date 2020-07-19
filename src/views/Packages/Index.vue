b2f9f91bae9ceb69f360f86d850fe4558c43868a
Sarang <sarang98divakaran@gmail.com>
2020-07-19 13:55:21 +0530

    added packages component

---
+++ src/views/Packages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container-fluid pt-4 pt-md-7">
+        <div class="row">
+            <div class="col-12">
+                <router-view />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Packages',
+};
+</script>
---
+++ src/views/Packages/Pages/Packages.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="card shadow">
+        <div class="card-header d-flex justify-content-between">
+            <h3>Packages</h3>
+        </div>
+        <div class="card-body p-0 position-relative min__height">
+            <div class="p-2 d-flex justify-content-between">
+                <div>
+                    <small class="mr-1 font-weight-bold">Per Page:</small>
+                    <base-dropdown>
+                        <base-button slot="title" size="sm" icon="caret-down" icon-position="right">
+                            {{ perPage }}
+                        </base-button>
+                        <a class="dropdown-item" @click="perPage = 10">10</a>
+                        <a class="dropdown-item" @click="perPage = 50">50</a>
+                        <a class="dropdown-item" @click="perPage = 100">100</a>
+                    </base-dropdown>
+                </div>
+                <div>
+                    <base-button icon="plus" size="sm" type="success" @click="addModal = true">
+                        Add Package
+                    </base-button>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <base-table
+                    class="table align-items-center table-flush service-table"
+                    thead-classes="thead-dark"
+                    :data="packages"
+                >
+                    <template slot="columns">
+                        <th class="text-center text-white">Package Name</th>
+                        <th class="text-center text-white">Price</th>
+                        <th class="text-center text-white">Product Count</th>
+                        <th class="text-center text-white">Duration (days)</th>
+                        <th class="text-center text-white">Actions</th>
+                    </template>
+                    <template slot-scope="{ row }">
+                        <td class="text-left name d-flex">
+                            {{ row.name }}
+                        </td>
+                        <td class="text-center">
+                            {{ row.price }}
+                        </td>
+                        <td class="text-center">
+                            {{ row.count }}
+                        </td>
+                        <td class="text-center">
+                            {{ row.duration }}
+                        </td>
+                        <td>
+                            <div class="d-flex justify-content-center">
+                                <base-button type="success" icon="eye" size="sm"></base-button>
+                                <base-button type="primary" icon="edit" size="sm"></base-button>
+                                <base-button type="danger" icon="trash" size="sm"></base-button>
+                            </div>
+                        </td>
+                    </template>
+                    <div slot="caption" class="col-12 p-5 d-flex justify-content-center align-items-center">
+                        <div>
+                            <span class="text-sm p-2">no packages</span>
+                            <font-awesome-icon icon="box-open" />
+                        </div>
+                    </div>
+                </base-table>
+            </div>
+            <div v-if="loading" class="over__lay">
+                <loading color="dark" />
+            </div>
+        </div>
+        <div v-if="totalPages" class="card-footer">
+            <base-pagination v-model="page" :page-count="totalPages" align="center" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        perPage: 10,
+        totalPages: null,
+        loading: null,
+        packages: null,
+    }),
+};
+</script>
