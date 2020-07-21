2d847be1d9b8c66854b67df3fb3c4fb944967c72
Sarang <sarang98divakaran@gmail.com>
2020-07-21 18:24:21 +0530

    store address

--- src/views/Settings/Pages/Stores/StoreDetails.vue
+++ src/views/Settings/Pages/Stores/StoreDetails.vue
@@ -13,15 +13,73 @@
                 </base-button>
             </div>
         </div>
-        <div class="card-body position-relative min__height">
-            <div class="row">
+        <div class="card-body position-relative min__height bg-secondary">
+            <div :class="['row', { 'mb-3': edit }]">
                 <div class="col-12">
                     <base-input
                         v-model="store.name"
                         :disabled="!edit"
                         label="Store Name"
-                        :input-classes="['form-control-alternative', { 'bg-white shadow-none': !edit }]"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
                         :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        class="m-0"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <span class="col-12 text-muted mt-4 heading-small font-weight-bold">Store Address</span>
+            </div>
+            <div :class="['row pl-md-4', { 'mb-3': edit }]">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <base-input
+                        v-model="store.area"
+                        :disabled="!edit"
+                        label="Area"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        :class="[{ 'm-0': !edit }]"
+                    />
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <base-input
+                        v-model="store.city"
+                        :disabled="!edit"
+                        label="City"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        :class="[{ 'm-0': !edit }]"
+                    />
+                </div>
+            </div>
+            <div :class="['row pl-md-4', { 'mb-3': edit }]">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <base-input
+                        v-model="store.district"
+                        :disabled="!edit"
+                        label="District"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        :class="[{ 'm-0': !edit }]"
+                    />
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <base-input
+                        v-model="store.state"
+                        :disabled="!edit"
+                        label="State"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        :class="[{ 'm-0': !edit }]"
+                    />
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <base-input
+                        v-model="store.pincode"
+                        :disabled="!edit"
+                        label="Pincode"
+                        :input-classes="['form-control-alternative', { 'bg-secondary shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                        :class="[{ 'm-0': !edit }]"
                     />
                 </div>
             </div>
@@ -47,6 +105,8 @@ export default {
     },
     methods: {
         async getStore(storeId) {
+            this.loading = true;
+
             try {
                 const response = await this.$axios({
                     method: 'get',
@@ -56,6 +116,7 @@ export default {
 
                 this.store = Object.assign({}, response.data.store);
                 this.original = Object.assign({}, response.data.store);
+                console.log(this.store);
             } catch (err) {
                 const res = err.response;
                 if (res && res.status >= 400 && res.status < 500 && res.data.error) {
@@ -64,6 +125,8 @@ export default {
                     this.$error('Something went wrong. Please try again later.');
                 }
             }
+
+            this.loading = false;
         },
         close() {
             this.store = Object.assign({}, this.original);
