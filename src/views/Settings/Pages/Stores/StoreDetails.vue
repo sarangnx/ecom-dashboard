a93a2be20a24d15481369c31b34a4a239d356e80
Sarang <sarang98divakaran@gmail.com>
2020-07-21 16:58:01 +0530

    Get store edit/save button different styles for edit mode

--- src/views/Settings/Pages/Stores/StoreDetails.vue
+++ src/views/Settings/Pages/Stores/StoreDetails.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="card shadow">
-        <div class="card-header d-flex justify-content-start align-items-center">
-            <base-button size="sm" icon="arrow-left" @click="$router.push('/settings/stores')"></base-button>
-            <h3 class="m-0">Store</h3>
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <div class="d-flex">
+                <base-button size="sm" icon="arrow-left" @click="$router.push('/settings/stores')"></base-button>
+                <h3 class="m-0">Store</h3>
+            </div>
+            <base-button v-if="!edit" type="primary" size="sm" @click="edit = true">Edit</base-button>
+            <div v-else>
+                <base-button v-show="!loading" size="sm" type="danger" icon="times" @click="close"></base-button>
+                <base-button :loading="loading" :disabled="loading" size="sm" type="success" icon="save" @click="save">
+                    Save
+                </base-button>
+            </div>
         </div>
         <div class="card-body position-relative min__height">
-            <div v-if="!loading" class="over__lay">
+            <div class="row">
+                <div class="col-12">
+                    <base-input
+                        v-model="store.name"
+                        :disabled="!edit"
+                        label="Store Name"
+                        :input-classes="['form-control-alternative', { 'bg-white shadow-none': !edit }]"
+                        :classes="['d-flex', { 'flex-row align-items-baseline': !edit }, { 'flex-column': edit }]"
+                    />
+                </div>
+            </div>
+            <div v-if="loading" class="over__lay">
                 <loading />
             </div>
         </div>
@@ -15,6 +35,41 @@
 export default {
     data: () => ({
         loading: false,
+        edit: false,
+        store: {},
+        original: null,
     }),
+    mounted() {
+        const storeId = this.$route.params ? this.$route.params.storeId : null;
+        if (storeId) {
+            this.getStore(storeId);
+        }
+    },
+    methods: {
+        async getStore(storeId) {
+            try {
+                const response = await this.$axios({
+                    method: 'get',
+                    url: '/stores/store',
+                    params: { storeId },
+                });
+
+                this.store = Object.assign({}, response.data.store);
+                this.original = Object.assign({}, response.data.store);
+            } catch (err) {
+                const res = err.response;
+                if (res && res.status >= 400 && res.status < 500 && res.data.error) {
+                    this.$error(res.data.error.message);
+                } else {
+                    this.$error('Something went wrong. Please try again later.');
+                }
+            }
+        },
+        close() {
+            this.store = Object.assign({}, this.original);
+            this.edit = false;
+        },
+        async save() {},
+    },
 };
 </script>
